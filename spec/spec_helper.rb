569e35e197951ddece0f0d4671442ce8c2364852
Ernie Brodeur <ebrodeur@ujami.net>
2018-08-26 21:45:56 -0700

    added persistence

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'pry'
 RSpec.configure do |config|
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
+  config.example_status_persistence_file_path = "tmp/examples.txt"
 end
 
 SimpleCov.formatters = [
